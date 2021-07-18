8117dc3c7cdcb68f5f2c3d8755f9ad9e222cfe85
Lasyanga <74501625+Lasyanga@users.noreply.github.com>
2021-07-18 15:21:05 +0800

    Simple Calculator

--- Calculator.java
+++ Calculator.java
@@ -87,7 +87,7 @@ class Calculator{
 				System.out.print("\nDo you want to perform another Operation? y/n: "); 
 				option = in.readLine();
 
-				if(option.equalsIgnoreCase("y")){ // if the user type y the program will re-run
+				if(option.equalsIgnoreCase("y")){
 					again = true;
 				}else{
 					System.exit(0);
